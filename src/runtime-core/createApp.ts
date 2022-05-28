f8c617e495282fbdd53bd23d32a49fb53185ea22
plasticine9750 <975036719@qq.com>
2022-05-28 10:36:24 +0800

    feat(runtime-core): mount supports  input strings

--- src/runtime-core/createApp.ts
+++ src/runtime-core/createApp.ts
@@ -7,6 +7,10 @@ export function createApp(rootComponent) {
       // 先将 rootComponent 转成 VNode 再进行处理
       const vnode = createVNode(rootComponent);
 
+      if (typeof rootContainer === 'string') {
+        rootContainer = document.querySelector(rootContainer);
+      }
+
       render(vnode, rootContainer);
     },
   };
