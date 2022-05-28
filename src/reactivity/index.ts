d949db625b303329e56ac24533ec7740e699dd65
plasticine9750 <975036719@qq.com>
2022-05-28 11:33:59 +0800

    build: bundle reactivity

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -1,3 +1,14 @@
-export function add(a, b) {
-  return a + b;
-}
+export {
+  reactive,
+  readonly,
+  shallowReadonly,
+  isReadonly,
+  isReactive,
+  isProxy,
+} from './reactive';
+
+export { ref, unref, proxyRefs, isRef } from './ref';
+
+export { effect, stop } from './effect';
+
+export { computed } from './computed';
